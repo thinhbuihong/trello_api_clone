4aeb423d8c4a6343fab4e91f7b24d9c09858168d
thinh bui <thinhbuihong.ma@gmail.com>
2022-04-24 09:26:14 +0700

    tg

--- src/app.ts
+++ src/app.ts
@@ -38,7 +38,7 @@ app.use("/api/boards", requireAuth, boardRouter);
 app.use("/api/lists", requireAuth, listRouter);
 app.use("/api/cards", requireAuth, cardRouter);
 app.use("/hi", (_req, res) => {
-  res.json("hello hi 3");
+  res.json("hello hi 3 4");
 });
 
 app.use("/", notFound);
